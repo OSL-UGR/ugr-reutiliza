4087e48f4d90603daa3c00298c41c6f0d5644aaa
Parallex <alux6mc@gmail.com>
2024-07-03 11:39:08 +0200

    Added features
    + Added functionality to reserve several pieces of furniture
    + Implemented the option to have several claimants
    + Added unbook feature
    + Displaying the left ammount of furnitures

--- muebles/landing/views.py
+++ muebles/landing/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from .backends import SettingsBackend
-from .models import Mueble, Foto, Usuario, categorias as cat
+from .models import Mueble, Foto, Usuario, Reserva, categorias as cat
 
 URL = 'muebles/'
 backend = SettingsBackend()
@@ -78,11 +78,43 @@ def loginPage(request):
 
 
 @login_required
+def unbookMueble(request, mueble_id):
+    mueble = Mueble.objects.get(pk=mueble_id)
+    reserva = Reserva.objects.get(mueble=mueble, demandante=request.user)
+
+    if (request.method == "POST"):
+        reserva.delete()
+        return redirect(f"/{URL}{mueble_id}/post")
+    else:
+        return redirect("index")
+
+
+@login_required
 def bookMueble(request, mueble_id):
     mueble = Mueble.objects.get(pk=mueble_id)
-    if (request.method == "POST" and mueble.demandante is None):
-        mueble.demandante = request.user
-        mueble.save()
+    reservas = Reserva.objects.filter(mueble=mueble)
+
+    peticion = int(request.POST['cantRes'])
+    existente = False
+
+    total = 0
+    for reserva in reservas:
+        if (reserva.demandante == request.user):
+            reservaUser = reserva
+            existente = True
+        total += reserva.cantidad
+
+    restantes = mueble.cantidad - total
+
+    if (request.method == "POST" and restantes - peticion >= 0):
+        if (existente):
+            reserva = reservaUser
+            reserva.cantidad += peticion
+        else:
+            reserva = Reserva(mueble=mueble, cantidad=peticion,
+                              demandante=request.user)
+        reserva.save()
+
         return redirect(f"/{URL}{mueble_id}/post")
     else:
         return redirect("index")
@@ -198,7 +230,19 @@ def perfil(request):
 def post(request, mueble_id):
     mueble = Mueble.objects.get(pk=mueble_id)
     ofertante = mueble.ofertante
-    demandante = mueble.demandante
+    reservas = Reserva.objects.filter(mueble=mueble)
+
+    total = 0
+    demandantes = []
+    for reserva in reservas:
+        demandantes.append(reserva.demandante)
+        total += reserva.cantidad
+
+    try:
+        reserva = Reserva.objects.get(mueble=mueble, demandante=request.user)
+    except:
+        reserva = None
+
     imagenes = [mueble.main_image]
     fotos = Foto.objects.filter(mueble=mueble)
     usuario = Usuario.objects.get(email=request.user)
@@ -206,9 +250,12 @@ def post(request, mueble_id):
     for foto in fotos:
         imagenes.append(foto.imagen)
     context = {
+            'restantes': mueble.cantidad - total,
             'mueble': mueble,
             'ofertante': ofertante,
-            'demandante': demandante,
+            'reservas': reservas,
+            'reserva': reserva,
+            'demandantes': demandantes,
             'images': imagenes,
             'user': usuario,
             "URL": URL
